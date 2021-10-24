c7cfc7101914bb8c9366191541b9062647cc0fcf
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-24 17:29:12 -0500

    set up basic GET routes for index and notes

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const router = require('express').Router();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
+const { notes } = require('./db/db');
 
-app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+router.get('/api/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
