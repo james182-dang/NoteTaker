89a1d64369fe584300e75a8a71c55dd63b68cc01
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-24 17:53:43 -0500

    POST functionality & middleware

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const router = require('express').Router();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db');
+const { notes } = require('./db/db.json');
+
+function createNewNote(body, notes) {
+    const note = body;
+    notes.push(note);
+
+    return note;
+}
+
+//Middleware functionality
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
@@ -13,19 +25,27 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-
-router.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
 
+
+
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// });
+
+
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // add note to json file and notes array
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 })
 
 app.listen(PORT, () => {
