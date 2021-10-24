807b70eb15aff4fedad322a7b08ea5c4c9c2f65c
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-24 18:19:29 -0500

    uncomment wildcard route

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.get('/api/notes', (req, res) => {
 
 
 
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'))
-// });
+// Wildcard Route!
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 
 app.post('/api/notes', (req, res) => {
