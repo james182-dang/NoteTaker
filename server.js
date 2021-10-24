1d5596c0a2298e7ea452a7b8b134b226accf0a23
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-24 18:10:22 -0500

    set up static route

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
